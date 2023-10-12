c02615c79c9fdea6eb821f2d5e8e1ff4950d8eba
Luvyjakk <slonik_zloy@mail.ru>
2023-10-12 23:01:54 +0300

    aa

---
+++ cards.js
@@ -0,0 +1,131 @@
+$ ('.card').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card1').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg1').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg1').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card2').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg2').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg2').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card3').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg3').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg3').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card4').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg4').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg4').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card5').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg5').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg5').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card6').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg6').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg6').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card7').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg7').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg7').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card8').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg8').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg8').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card9').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg9').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg9').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
+
+$ ('.card10').click(function(e) {
+    e.preventDefault();
+    $('.popup-bg10').fadeIn(600);
+    $('html').addClass('no-scroll');
+});
+
+$ ('.close-popup').click(function() {
+    $('.popup-bg10').fadeOut(600);
+    $('html').removeClass('no-scroll');
+});
+
